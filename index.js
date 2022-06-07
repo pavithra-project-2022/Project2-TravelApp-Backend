6075f59fd1d41e70e68442937db5b099e0b29caf
Pavithra Sethuraman <pavithrasethuraman92@gmail.com>
2022-06-08 02:25:07 +0800

    make it better

--- index.js
+++ index.js
@@ -6,14 +6,13 @@ const userRoute = require("./routes/users");
 const pinRoute = require("./routes/pins");
 const cors = require("cors")
 
-
 dotenv.config();
 app.use(cors())
 
 app.use(express.json());
-const URI = "mongodb+srv://pavi:pavi@cluster0.ydkuj.mongodb.net/mapapp?retryWrites=true&w=majority";
+
 mongoose 
- .connect(URI, {
+ .connect(process.env.MONGO_URL, {
         useNewUrlParser: true,
         useUnifiedTopology: true,
           })   
