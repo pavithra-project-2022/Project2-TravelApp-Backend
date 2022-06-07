da6ac5346e71e03311dd7baca6c1dce11832c30b
Pavithra Sethuraman <pavithrasethuraman92@gmail.com>
2022-06-08 02:28:54 +0800

    make it better

--- index.js
+++ index.js
@@ -12,9 +12,10 @@ app.use(cors({
 }))
 
 app.use(express.json());
+const URL = "mongodb+srv://pavi:pavi@cluster0.ydkuj.mongodb.net/mapapp?retryWrites=true&w=majority"
 
 mongoose 
- .connect(process.env.MONGO_URL, {
+ .connect(URL, {
         useNewUrlParser: true,
         useUnifiedTopology: true,
         
